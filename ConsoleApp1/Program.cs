65105086c19bb1f9997d58cf8d9e68d6770898d2
sosobraldev7 <sosobral.tech@gmail.com>
2025-11-13 13:45:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿     float nota1, nota2, mediafinal;
+   Console.WriteLine("Digite a primeira nota: ");
+nota1 = float.Parse(Console.ReadLine()!);
+   Console.WriteLine("Digite a segunda nota: ");
+     nota2 = float.Parse(Console.ReadLine()!);
+        mediafinal = (nota1 + nota2) / 2;
+if (mediafinal <= 6)
+{
+    Console.WriteLine("Reprovado");
+}
+    else if (mediafinal <= 7)
+{
+    Console.WriteLine("Recuperação");
+} 
+    else 
+{
+    Console.WriteLine("Aprovado");
+}
